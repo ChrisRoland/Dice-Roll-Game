e55428f9607a5706f33633341f059f9ac88e5a60
ChrisRoland <chrisebuberoland@gmail.com>
2024-05-25 11:22:07 +0100

    add: updates

--- index.js
+++ index.js
@@ -1,32 +1,3 @@
-// const randomNumber1 = Math.floor(Math.random() * 6) + 1;
-
-// const randomNumber2 = Math.floor(Math.random() * 6) + 1;
-
-// let img1 = document.querySelector(".img1");
-
-// let img2 = document.querySelector(".img2");
-
-// // Set the src attribute to the corresponding dice image
-// img1.setAttribute("src", "images/dice" + randomNumber1 + ".png");
-
-// img2.setAttribute("src", "images/dice" + randomNumber2 + ".png");
-
-// //Display winner
-// if (randomNumber1 > randomNumber2) {
-//     document.querySelector("h1").textContent = "🚩 Player 1 Wins!";
-
-// } else if (randomNumber1 < randomNumber2) {
-//     document.querySelector("h1").textContent = "Player 2 Wins! 🚩";
-
-// } else if (randomNumber1 === randomNumber2) {
-//     document.querySelector("h1").textContent = "It's a Draw! 🏴‍☠️";
-// }
-
-// // Add Button to roll die
-// document.getElementById("refreshButton").addEventListener("click", function() {
-//     location.reload(); // Refresh the page
-// });
-
 function rollDice() {
   // Generate random numbers between 1 and 6 for both players
   const randomNumber1 = Math.floor(Math.random() * 6) + 1;
